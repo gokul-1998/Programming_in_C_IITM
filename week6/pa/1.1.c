300210dd2b273a8c8cc2c2839e0d80c3793b30f6
gokul-1998 <gokulakrishnanm1998@gmail.com>
2026-01-03 17:07:09 +0530

    jhuh

---
+++ week6/pa/1.1.c
@@ -0,0 +1,15 @@
+// write a C program to print diff datatypes, 
+    
+
+#include <stdio.h>
+int main() {
+    int age = 10;
+    char name[] = "John";
+    float income = 2000.098;
+    // pointer
+
+
+//    your code goes here
+
+    return 0;
+}
